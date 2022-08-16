efb0ce0c2cbf7c0713a39c65188d13d8355bdfbc
Sergey <pavbatol@yandex.ru>
2022-08-16 09:09:30 +0400

    Sprint_8: feat: Попытаться сериализовать менеджер

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import api.HttpTaskServer;
 import api.KVServer;
 import api.KVTaskClient;
+import com.google.gson.Gson;
 import managers.FileBackedTaskManager;
 import tasks.Epic;
 import tasks.Subtask;
@@ -19,18 +20,18 @@ public class Main {
 //        String[] pathElements= new String[]{"resources", "back.csv"};
 //        FileBackedTaskManager.main(pathElements);
 
-        FileBackedTaskManager fbtm = Managers.getNewFileBackedTaskManager();
+        FileBackedTaskManager fbtm1 = Managers.getNewFileBackedTaskManager();
 
         Epic epic1 = new Epic("name0", "description0");
         Epic epic2 = new Epic("name0", "description0");
-        fbtm.addEpic(epic1);
-        fbtm.addEpic(epic2);
+        fbtm1.addEpic(epic1);
+        fbtm1.addEpic(epic2);
         Task task1 = new Task("name1", "description1", NEW);
         Task task2 = new Task("name2", "description2", NEW);
         Subtask subtask1 = new Subtask("name4", "description4", NEW, epic1.getId());
         Subtask subtask2 = new Subtask("name5", "description5", NEW, epic1.getId());
 
-        final int timeStep = fbtm.getTimeStepByTimeManager();
+        final int timeStep = fbtm1.getTimeStepByTimeManager();
         LocalDateTime start = LocalDateTime.of(
                 LocalDate.now().getYear(),
                 LocalDate.now().getMonth(),
@@ -47,21 +48,19 @@ public class Main {
         subtask2.setStartTime(task2.getEndTime());
         subtask1.setStartTime(subtask2.getEndTime());
 
-        fbtm.addTask(task1);
-        fbtm.addTask(task2);
-        fbtm.addSubtask(subtask1);
-        fbtm.addSubtask(subtask2);
+        fbtm1.addTask(task1);
+        fbtm1.addTask(task2);
+        fbtm1.addSubtask(subtask1);
+        fbtm1.addSubtask(subtask2);
 
-        //System.out.println(fbtm);
-
-        fbtm.getTaskById(task1.getId());
-        //fbtm.getTaskById(task2.getId());
-        fbtm.getSubtaskById(subtask1.getId());
-        //fbtm.getSubtaskById(subtask2.getId());
+        fbtm1.getTaskById(task1.getId());
+        //fbtm1.getTaskById(task2.getId());
+        fbtm1.getSubtaskById(subtask1.getId());
+        //fbtm1.getSubtaskById(subtask2.getId());
 
         HttpTaskServer httpTaskServer;
         try {
-            httpTaskServer = new HttpTaskServer(fbtm);
+            httpTaskServer = new HttpTaskServer(fbtm1);
         } catch (IOException e) {
             System.out.println("Не удалось создать сервер");
             return;
@@ -69,12 +68,43 @@ public class Main {
         //httpTaskServer.start();
         //httpTaskServer.stop();
 
-        //*******************************************************
-        //*******************************************************
-        //*******************************************************
+        //----------------
+        KVServer server = null;
+        KVTaskClient client = null;
+        try {
+            server = new KVServer();
+            server.start();
+        } catch (IOException e) {
+            System.out.println("Не удалось запустить HTTP-Server\n" + e.getMessage());
+        }
+        try {
+            client = new KVTaskClient("http://localhost:8078");
+        } catch (RuntimeException e) {
+            System.out.println("Не удалось запустить HTTP-Client\n" + e.getMessage());
+        }
 
-        new KVServer().start();
-        KVTaskClient client = new KVTaskClient("http://localhost:8078");
+        System.out.println("Менеджер-оригинал");
+        System.out.println(fbtm1);
+
+        // TODO: 16.08.2022 Нужен адаптер для GSON на:
+        //  CustomLinkedList,
+        //  Path
+        Gson gson = new Gson();
+        String json;
+        json = gson.toJson(fbtm1);
+//        json = gson.toJson(task1);
+        System.out.println(json);
+
+//        if (client != null) {
+//            FileBackedTaskManager fbtm2 = null;
+//            String key1 = "put1";
+//            client.put(key1, json);
+//            String response = client.load(key1);
+//            fbtm2 = gson.fromJson(response, FileBackedTaskManager.class);
+//            System.out.println("Менеджер из восстановления");
+//            System.out.println(fbtm2);
+//        }
+
 
     }
 }
