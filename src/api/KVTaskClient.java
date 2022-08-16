b27140318ba26cc0a41cb47f4adcbd8c5bf10979
Sergey <pavbatol@yandex.ru>
2022-08-16 07:52:38 +0400

    Sprint_8: feat: Завершить реализацию

--- src/api/KVTaskClient.java
+++ src/api/KVTaskClient.java
@@ -5,41 +5,30 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.ByteBuffer;
-import java.util.concurrent.Flow;
 
 public class KVTaskClient {
-    private final String url;
+    private final String host;
     private final HttpClient client;
     private final String apiToken;
 
-    public KVTaskClient(String url) throws RuntimeException{
-        this.url = url;
+    public KVTaskClient(String host) throws RuntimeException{
+        this.host = host;
         this.client = HttpClient.newHttpClient();
         apiToken = getApiToken();
         if (apiToken.isEmpty()) {
             System.out.println("Не получен токен");
-            throw new RuntimeException();
+            throw new RuntimeException("Не удалось получить API_TOKEN");
         }
         System.out.println("Ваш API_TOKEN: " + apiToken);
     }
 
     void put(String key, String json) {
         // POST /save/<ключ>?API_TOKEN=
-        URI uri = URI.create(url + "/save" + key + "?API_TOKEN=" + apiToken);
+        URI uri = URI.create(host + "/save/" + key + "?API_TOKEN=" + apiToken);
+        final HttpRequest.BodyPublisher body = HttpRequest.BodyPublishers.ofString(json);
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(uri)
-                .POST(new HttpRequest.BodyPublisher() {
-                    @Override
-                    public long contentLength() {
-                        return 0;
-                    }
-
-                    @Override
-                    public void subscribe(Flow.Subscriber<? super ByteBuffer> subscriber) {
-
-                    }
-                })
+                .POST(body)
                 .build();
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -56,31 +45,8 @@ public class KVTaskClient {
 
     String load(String key) {
         //GET /load/<ключ>?API_TOKEN=
-        URI uri = URI.create(url + "/save" + key + "?API_TOKEN=" + apiToken);
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(uri)
-                .GET()
-                .build();
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
-                System.out.println("Данные сохранены");
-            } else {
-                System.out.println("Что-то пошло не так. Сервер вернул код состояния: " + response.statusCode());
-            }
-        } catch (IOException | InterruptedException e) {
-            System.out.println("Во время отправки запроса возникла ошибка.\n" +
-                    "Проверьте, пожалуйста, адрес и повторите попытку.");
-        }
-    }
-
-
-        return ...
-    }
-
-    private String getApiToken() {
         String result = "";
-        URI uri = URI.create(url + "/register");
+        URI uri = URI.create(host + "/load/" + key + "?API_TOKEN=" + apiToken);
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(uri)
                 .GET()
@@ -89,6 +55,29 @@ public class KVTaskClient {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             if (response.statusCode() == 200) {
                 result = response.body();
+                System.out.println("Данные получены");
+            } else {
+                System.out.println("Что-то пошло не так. Сервер вернул код состояния: " + response.statusCode());
+            }
+        } catch (IOException | InterruptedException e) {
+            System.out.println("Во время отправки запроса возникла ошибка.\n" +
+                    "Проверьте, пожалуйста, адрес и повторите попытку.");
+        }
+        return result;
+    }
+
+    private String getApiToken() {
+        String result = "";
+        URI uri = URI.create(host + "/register");
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(uri)
+                .GET()
+                .build();
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                result = response.body();
+                System.out.println("Регистрация успешна");
             } else {
                 System.out.println("Что-то пошло не так. Сервер вернул код состояния: " + response.statusCode());
             }
@@ -98,16 +87,5 @@ public class KVTaskClient {
         }
         return result;
     }
-
-//    protected String readText(HttpExchange h) throws IOException {
-//        return new String(h.getRequestBody().readAllBytes(), UTF_8);
-//    }
-//
-//    protected void sendText(HttpExchange h, String text) throws IOException {
-//        byte[] resp = text.getBytes(UTF_8);
-//        h.getResponseHeaders().add("Content-Type", "application/json");
-//        h.sendResponseHeaders(200, resp.length);
-//        h.getResponseBody().write(resp);
-//    }
 
 }
