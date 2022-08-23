991d2e8c09c46420c926046c30e4d022f07d2009
Sergey <pavbatol@yandex.ru>
2022-08-23 22:28:46 +0400

    Sprint_8: style: Удалить TODO

--- src/util/CSVConverter.java
+++ src/util/CSVConverter.java
@@ -15,7 +15,6 @@ public final class CSVConverter {
     private CSVConverter() {
     }
 
-    // TODO: 21.08.2022 Учесть userId
     public static String toString(Task task) {
         if (task == null) {
             System.out.println("Перевод в строку НЕ выполнен, объект не инициализирован");
@@ -60,7 +59,6 @@ public final class CSVConverter {
         return sb.toString();
     }
 
-    // TODO: 21.08.2022 Учесть userId
     public static Task fromStringOfTask(String value) {
         if (value == null) {
             System.out.println("Перевод строки в задачу НЕ выполнен, строка = null");
@@ -133,7 +131,6 @@ public final class CSVConverter {
         return new ArrayList<>();
     }
 
-    // TODO: 21.08.2022 Учесть userId
     public static String getHeads() {
         return "id," +
                 "userId," +
